1b6aca88770d678addcc77b69752c0549881a803
himanshu chhatbar <himanshu.chhatbar@gmail.com>
2026-03-12 13:03:46 +0000

    hero prop

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 10707674
+    "domain": "5gvci.com",
+    "zoneId": 10719832
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
         <meta name="keywords" content="Daily Expense Tracker, Pocket Tracker" />
         <meta name="google-adsense-account" content="ca-pub-7525000535202715" />
-        <Script 
-          src="https://5gvci.com/act/files/tag.min.js?z=10719775" 
-          data-cfasync="false" 
-          async 
-          strategy="afterInteractive"
-        />
+        <script src="https://quge5.com/88/tag.min.js" data-zone="219012" async data-cfasync="false"></script>
       </head>
       <body className="font-body antialiased bg-gray-200">
         {children}
